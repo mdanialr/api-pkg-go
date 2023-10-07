8242bb2fcd694f872c2f2716f65462cacd3966b3
mdanialr <mdanialrma@gmail.com>
2023-10-07 23:55:57 +0700

    feat: define log contract along with default no-op implementer

---
+++ logger/log_test.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewNop(t *testing.T) {
+	t.Run("Should have nop Log type", func(t *testing.T) {
+		nl := NewNop()
+		assert.NotNil(t, nl)
+		assert.Equal(t, &nopLog{}, nl)
+		assert.IsType(t, &nopLog{}, nl)
+	})
+	t.Run("Should do nothing", func(t *testing.T) {
+		nl := NewNop()
+		assert.NotNil(t, nl)
+		assert.Nil(t, nl.With())
+
+		// just run it, since its just do literary nothing
+		nl.Init(time.Microsecond)
+		nl.Flush(time.Microsecond)
+		nl.Dbg("")
+		nl.Inf("")
+		nl.Wrn("")
+		nl.Err("")
+	})
+}
